09a59837684633420267abc8b874fc568396846a
Myagmarsuren <mimiigaa3@gmail.com>
2022-12-07 01:37:32 +0800

    json test, product card create and styling

--- js/product-card.js
+++ js/product-card.js
@@ -1,21 +1,23 @@
-class ProductCard extends HTMLElement {
-        // constructor() {
-        //         super();
-        // }
-        connectedCallback() {
-                this.src = this.getAttribute('src');
-                this.alt = this.getAttribute('alt');
-                this.text = this.getAttribute('text');
-                this.price = this.getAttribute('price');
-
-                this.parentNode.innerHTML = `
-                <div class="product-card">
+class ProductCardItem{
+        constructor(src, alt, name, price) {
+                this.src = src;
+                this.alt = alt;
+                this.name = name;
+                this.price = price;
+        }
+        render() {
+                return `
+                <div class="product-cart">
                         <img src="${this.src}" alt="${this.alt}">
                         <h4>${this.text}</h4>
-                        <p class="product-card-price">${this.price}</p>
+                        <p class="product-cart-price">${this.price}</p>
                 </div>
                 `;
         }
 }
 
-customElements.define("gs25-product-card", ProductCard);
+export default class ProductCard{
+        constructor(){
+
+        }
+}
