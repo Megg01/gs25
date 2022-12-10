ecd8e6988a90f26e8174a98c2ac725e6c1ec4e36
Myagmarsuren <mimiigaa3@gmail.com>
2022-12-11 01:59:42 +0800

    slideshow image hiiv, js1 daalgavriig guitsetgesen

--- js/product-card.js
+++ js/product-card.js
@@ -8,7 +8,7 @@ class ProductCardItem{
         render1() {
                 return `
                 <div class="product-card">
-                        <img src="${this.src}" alt="${this.alt}" width="212px" height="212px">
+                        <img src="${this.src}" alt="${this.alt}" width="180px" height="180px">
                         <h4>${this.name}</h4>
                         <div class="product-card-price">
                                 <p>${this.price}</p>
@@ -24,10 +24,8 @@ function App(datas, targetElement) {
             let productObj = new ProductCardItem(data.src, data.alt, data.name, data.price);
             retVal += productObj.render1();
         }
-        document.getElementByClassId(targetElement).insertAdjacentHTML("beforeend", retVal);
+        document.getElementsByClassName(targetElement)[0].insertAdjacentHTML("beforeend", retVal);
 }
-window.onload = () => App(ProductCardData, "product-normal-cards");
-
 
 const ProductCardData =
 [
@@ -62,3 +60,75 @@ const ProductCardData =
                 "price": "1,600₮"
         }
 ];
+
+// Uramshuulaltai buteegdehuuniih
+
+class ProductBonusCardItem{
+        constructor(src, alt, name, price) {
+                this.src = src;
+                this.alt = alt;
+                this.name = name;
+                this.price = price;
+        }
+        render() {
+                return `
+                <div class="product-bonus-card">
+                        <div></div>
+                        <img src="${this.src}" alt="${this.alt}" width="180px" height="180px">
+                        <h4>${this.name}</h4>
+                        <div class="product-bonus-card-price">
+                                <p>${this.price}</p>
+                        </div>                
+                </div>
+                `;
+        }
+}
+function AppBonus(bonusDatas, targetBonusElement) {
+        let retBonusVal = "";
+
+        for (const data of bonusDatas) {
+            let productBonusObj = new ProductBonusCardItem(data.src, data.alt, data.name, data.price);
+            retBonusVal += productBonusObj.render();
+        }
+        document.getElementsByClassName(targetBonusElement)[0].insertAdjacentHTML("beforeend", retBonusVal);
+}
+
+
+const ProductBonusCardData =
+[
+        {
+                "src": "img/product-img/product-bonus-chips1.webp",
+                "alt": "chips",
+                "name": "Чипс Realprice эрдэнэшиштэй",
+                "price": "7,900₮"
+        },
+        {
+                "src": "img/product-img/product-bonus-chocopie1.webp",
+                "alt": "chocopie",
+                "name": "Чокопай Crown гүзээлзгэнэтэй",
+                "price": "9,500₮"
+        },
+        {
+                "src": "img/product-img/product-bonus-drink1.webp",
+                "alt": "juice",
+                "name": "Жүүс YouUs Алим 330Мл",
+                "price": "1,400₮"
+        },
+        {
+                "src": "img/product-img/product-bonus-water.webp",
+                "alt": "water",
+                "name": "Ус Aqua Se 500Мл",
+                "price": "9,500₮"
+        },
+        {
+                "src": "img/product-img/product-bonus-honey.webp",
+                "alt": "honey",
+                "name": "Халбага зөгийн бал 10Гр",
+                "price": "1,600₮"
+        }
+];
+
+window.onload = () =>{
+        App(ProductCardData, "product-cards");
+        AppBonus(ProductBonusCardData, "product-bonus-cards");
+}
