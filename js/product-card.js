a889073c38cd8562de9ed8bf1c147279a0af2760
Myagmarsuren <mimiigaa3@gmail.com>
2022-12-12 00:03:59 +0800

    +navbar fixed position +product more test

--- js/product-card.js
+++ js/product-card.js
@@ -8,8 +8,12 @@ class ProductCardItem{
         render1() {
                 return `
                 <div class="product-card">
-                        <img src="${this.src}" alt="${this.alt}" width="180px" height="180px">
-                        <h4>${this.name}</h4>
+                        <div class="product-card-img-container">
+                                <img src="${this.src}" alt="${this.alt}" width="180px" height="180px">
+                        </div>
+                        <div class="product-card-name">
+                                <h4>${this.name}</h4>
+                        </div>
                         <div class="product-card-price">
                                 <p>${this.price}</p>
                         </div>
@@ -48,16 +52,40 @@ const ProductCardData =
                 "price": "22,000₮"
         },
         {
-                "src": "img/product-img/product-coffee1.webp",
-                "alt": "coffee",
-                "name": "Кафе25 Aмерикано",
-                "price": "3,500₮"
+                "src": "img/product-img/product-chicken2.webp",
+                "alt": "chicken",
+                "name": "Chicken25 MIX box",
+                "price": "22,000₮"
+        },
+        {
+                "src": "img/product-img/product-chicken3.webp",
+                "alt": "chicken",
+                "name": "Chicken25 /sauce/",
+                "price": "4,900₮"
         },
         {
                 "src": "img/product-img/product-pepsi1.webp",
                 "alt": "pepsi",
                 "name": "Pepsi аягатай",
                 "price": "1,600₮"
+        },
+        {
+                "src": "img/product-img/product-coffee1.webp",
+                "alt": "coffee",
+                "name": "Кафе25 Aмерикано",
+                "price": "3,500₮"
+        },
+        {
+                "src": "img/product-img/product-mantuunbuuz1.webp",
+                "alt": "mantuu",
+                "name": "Мантуун бууз / кимчи /",
+                "price": "1,800₮"
+        },
+        {
+                "src": "img/product-img/product-boortsog.webp",
+                "alt": "mantuu",
+                "name": "Боорцог / мушгиа /",
+                "price": "1,000₮"
         }
 ];
 
