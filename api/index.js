2fb1375d1fe5278795b08e7541bca4496353032a
Adembe <96032079+Adembe@users.noreply.github.com>
2023-01-09 16:12:55 +0800

    api code deerh tailbar

--- api/index.js
+++ api/index.js
@@ -6,8 +6,10 @@ const product = require("./module/products.js");
 const cors = require("cors");
 
 app.use(cors());
+// "/" duudagdhad shuud product ajlana
 app.use("/", product);
-app.use("/products", product);
+// "/product1plus1" duudagdhad shuud product ajlana
+app.use("/product1plus1", product);
 app.listen(port, () => {
   console.log(`Server is listening at http: //localhost: ${port}`);
 });
